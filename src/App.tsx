c504964784b607df9c819bb98e0c889d71566be8
nasii05 <iamnasih@gmail.com>
2025-03-27 11:56:32 +0530

    essential pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/layout";
 import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
 
 
 function App() {
@@ -9,7 +11,10 @@ function App() {
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark">
         <Layout>
-          Hello
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
         </Layout>
       </ThemeProvider>
     </BrowserRouter>
