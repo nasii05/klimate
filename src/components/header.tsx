2d1a30f9c2afff8157104e7a6608328049b99026
nasii05 <iamnasih@gmail.com>
2025-03-27 12:15:54 +0530

    theme toggle added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,31 @@
+import { useTheme } from "@/context/theme-provider";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header>
-    <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2">
-        <Link to={"/"}>
-            <img src="/logo.png" alt="Klimate Logo" className="h-14" />
-        </Link>
-    </div>
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
 
-    <div>
-        {/* search */}
-        {/* theme toggle */}
-    </div>
-  </header>
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img
+            src={theme === "dark" ? "/logo.png" : "/logo2.png"}
+            alt="Klimate logo"
+            className="h-14"
+          />
+        </Link>
+
+        <div className="flex gap-4">
+          {/* <CitySearch />
+        <ThemeToggle /> */}
+
+          <div onClick={() => setTheme(isDark ? "light" : "dark")}>toggle</div>
+        </div>
+      </div>
+    </header>
+  )
+
 }
 
 export default Header;
