63f8da4cd59f75edd9b9fc611cbbbe17daff5521
nasii05 <iamnasih@gmail.com>
2025-03-28 11:04:44 +0530

    toggle button update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -20,7 +21,16 @@ const Header = () => {
           {/* <CitySearch />
         <ThemeToggle /> */}
 
-          <div onClick={() => setTheme(isDark ? "light" : "dark")}>toggle</div>
+          <div onClick={() => setTheme(isDark ? "light" : "dark")}
+            className={`flex items-center cursor-pointer transition-transform duration-500
+            ${isDark ? "rotate-180" : "rotate-0"}` }>
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+            ) : (
+              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+            )
+            }
+          </div>
         </div>
       </div>
     </header>
